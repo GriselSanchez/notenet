f3d33c38ac9db181e6b4e15726f127761be8775b
grisel <griselalmasanchez@gmail.com>
2020-02-27 03:05:14 -0300

    mongodb 2

--- src/startup/db.js
+++ src/startup/db.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
 module.exports = function () {
-        mongoose
-            .connect(process.env.MONGODB_URI), {
-                useNewUrlParser: true
-            })
-    .then(() => console.log("Connected to database..."))
-    .catch(err => console.error("Could not connect to database.", err));
+    mongoose
+        .connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true
+        }).then(() => console.log("Connected to database..."))
+        .catch(err => console.error("Could not connect to database.", err));
 }
