cca8c1afa294d34d2804ff040261cb9962fac3bf
grisel <griselalmasanchez@gmail.com>
2020-02-27 03:19:08 -0300

    mongodb whitelist ip

--- src/startup/db.js
+++ src/startup/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = function () {
     mongoose
-        .connect(process.env.MONGODB_URI, {
+        .connect(process.env.MONGODB_URI || "mongodb://localhost/journal", {
             useNewUrlParser: true
         }).then(() => console.log("Connected to database..."))
         .catch(err => console.error("Could not connect to database.", err));
